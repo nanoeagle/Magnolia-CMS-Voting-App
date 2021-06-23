9a2c5d9ff1a09c674bf56d075b1bca6a8d275f3d
nanoeagle <fistrias.ikl@gmail.com>
2021-06-23 11:59:38 +0700

    FA-12: Change poll status.

---
+++ magnolia-voting-app/src/main/java/com/example/votingapp/util/PollUtils.java
@@ -0,0 +1,40 @@
+package com.example.votingapp.util;
+
+import java.util.*;
+
+import javax.jcr.*;
+
+import com.example.votingapp.enums.*;
+
+import org.slf4j.*;
+
+import info.magnolia.jcr.*;
+
+public class PollUtils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PollUtils.class);
+
+    public static Enum<PollStatus> pickPollStatus(Node source) throws RuntimeException {
+        Calendar today = Calendar.getInstance();
+
+        try {
+            Calendar startDate = source.getProperty("startDate").getValue().getDate();
+            Calendar expiryDate = source.getProperty("expiryDate").getValue().getDate();
+            
+            int todayStDateCompVal = today.compareTo(startDate);
+            int todayExDateCompVal = today.compareTo(expiryDate);
+
+            if (todayStDateCompVal < 0) {
+                return PollStatus.TO_DO;
+            }
+            if (todayStDateCompVal >= 0 && todayExDateCompVal < 0) {
+                return PollStatus.ON_GOING;
+            } 
+            return PollStatus.DONE;
+
+        } catch (RepositoryException e) {
+            LOGGER.error("RepositoryException found.", e);
+            throw new RuntimeRepositoryException(e);
+        }
+    }
+}
