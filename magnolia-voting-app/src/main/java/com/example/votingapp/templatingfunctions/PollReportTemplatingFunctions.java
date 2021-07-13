fd866646affff162ccbf0f5bd2bf973c8676dfc6
username-david <hothihoai9924@gmail.com>
2021-07-13 10:43:44 +0700

    FA-27 Configure module description for pollfn

---
+++ magnolia-voting-app/src/main/java/com/example/votingapp/templatingfunctions/PollReportTemplatingFunctions.java
@@ -0,0 +1,173 @@
+package com.example.votingapp.templatingfunctions;
+
+import info.magnolia.cms.security.JCRSessionOp;
+import info.magnolia.context.MgnlContext;
+import info.magnolia.jcr.util.NodeUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PollReportTemplatingFunctions {
+
+    private static final Logger log = LoggerFactory.getLogger(PollReportTemplatingFunctions.class);
+
+    private static String POLLS = "polls";
+    private static String SUM_QUESTIONS_NUMBER = "sumQuestionsNumber";
+    private static String SUM_ANSWERS_NUMBER = "sumAnswersNumber";
+    private static String SUM_VOTES_NUMBER = "sumVoteNumber";
+
+    Map<String, Integer> contentNodeMap = new HashMap<>();
+    Map<String, Integer> answerNodeMap = new HashMap<>();
+    Map root = new HashMap<>();
+
+    List<Node> questionList;
+    List<Node> answerList;
+    List<Node> voterList;
+
+    public String getAllChildren(String nodePath) {
+        questionList = new ArrayList<>();
+        answerList = new ArrayList<>();
+        voterList = new ArrayList<>();
+        try {
+            MgnlContext.doInSystemContext(new JCRSessionOp<Void>(POLLS) {
+                @Override
+                public Void exec(Session session) throws RepositoryException {
+                    Node parentNode = session.getNode("/" + nodePath);
+
+                    List<Node> childrenNodes = NodeUtil.asList(NodeUtil.collectAllChildren(parentNode));
+                    for (int i = 0; i < childrenNodes.size(); i++) {
+                        Node questionNode = childrenNodes.get(i);
+
+                        int level = getLevel(questionNode);
+                        if (level == 3) {
+                            questionList.add(questionNode);
+                        }
+                        if (level == 5) {
+                            answerList.add(questionNode);
+                        }
+                        if (level == 7) {
+                            voterList.add(questionNode);
+                        }
+                    }
+                    contentNodeMap.put(SUM_QUESTIONS_NUMBER, questionList.size());
+                    contentNodeMap.put(SUM_ANSWERS_NUMBER, answerList.size());
+                    contentNodeMap.put(SUM_VOTES_NUMBER, voterList.size() / questionList.size());
+
+                    countQuestionAndAnswerNumberByIndex();
+                    return null;
+                }
+            });
+        } catch (RepositoryException e) {
+            log.error("Could not get node {}.", e.getMessage(), e);
+        }
+        return null;
+    }
+
+    private void countQuestionAndAnswerNumberByIndex() throws RepositoryException {
+        root = new HashMap();
+        answerNodeMap = new HashMap<>();
+        for (int k = 0; k < questionList.size(); k++) {
+            int countOtherVote = 0;
+            for (int j = 0; j < answerList.size(); j++) {
+                Node answerNode = answerList.get(j);
+                List<Node> listAnswerByIndex = new ArrayList<>();
+                if (!isOtherAnswer(answerNode)) {
+                    addToList(listAnswerByIndex, j, k);
+                    contentNodeMap.put(SUM_ANSWERS_NUMBER + k + j, listAnswerByIndex.size());
+                    answerNodeMap.put(getAnswerContent(answerList.get(j)), listAnswerByIndex.size());
+                } else {
+                    addToList(listAnswerByIndex, j, k);
+                    countOtherVote += listAnswerByIndex.size();
+                }
+            }
+            contentNodeMap.put(SUM_ANSWERS_NUMBER + k + "Other", countOtherVote);
+            answerNodeMap.put("Other", countOtherVote);
+            root.put(k, answerNodeMap);
+        }
+    }
+
+    public List<ArrayList<String>> getVoteDetail() throws RepositoryException {
+        List<ArrayList<String>> voteDetailList = new ArrayList<ArrayList<String>>();
+        ArrayList<String> aVoteDetail = new ArrayList<>();
+        for (int i = 0; i < voterList.size(); i++) {
+            String name = voterList.get(i).getProperty("fullName").getValue().getString();
+            String email = voterList.get(i).getProperty("email").getValue().getString();
+            String answerValue = "";
+            String otherAnswerValue = "";
+            if (isOtherAnswer(voterList.get(i).getParent().getParent())) {
+                answerValue = "";
+                otherAnswerValue = getAnswerContent(voterList.get(i).getParent().getParent());
+            } else {
+                otherAnswerValue = "";
+                answerValue = getAnswerContent(voterList.get(i).getParent().getParent());
+            }
+            aVoteDetail.add(name);
+            aVoteDetail.add(email);
+            aVoteDetail.add(answerValue);
+            aVoteDetail.add(otherAnswerValue);
+            voteDetailList.add(aVoteDetail);
+            aVoteDetail = new ArrayList<>();
+        }
+        return voteDetailList;
+    }
+
+    public boolean isOtherAnswer(Node node) throws RepositoryException {
+        if (node.hasProperty("category")) {
+            return true;
+        }
+        return false;
+    }
+
+    private void addToList(List<Node> list, int j, int k) throws RepositoryException {
+        for (int i = 0; i < voterList.size(); i++) {
+            Node node = voterList.get(i);
+            String[] str = StringUtils.split(node.getPath(), "/");
+            if (str[4] != null && Integer.parseInt(str[4]) == j && str[2] != null && Integer.parseInt(str[2]) == k) {
+                list.add(node);
+            }
+        }
+    }
+
+    private int getLevel(Node node) throws RepositoryException {
+        String path = node.getPath();
+        String[] str = StringUtils.split(path, "/");
+        return str != null ? str.length : 0;
+    }
+
+    public int sumVotesNumber() {
+        if (contentNodeMap.containsKey(SUM_VOTES_NUMBER)) {
+            return contentNodeMap.get(SUM_VOTES_NUMBER);
+        }
+        return 0;
+    }
+
+    public int sumQuestionNumber() {
+        if (contentNodeMap.containsKey(SUM_QUESTIONS_NUMBER)) {
+            return contentNodeMap.get(SUM_QUESTIONS_NUMBER);
+        }
+        return 0;
+    }
+
+    public int sumAnswersNumber() {
+        if (contentNodeMap.containsKey(SUM_ANSWERS_NUMBER)) {
+            return contentNodeMap.get(SUM_ANSWERS_NUMBER);
+        }
+        return 0;
+    }
+
+    public String getAnswerContent(Node node) throws RepositoryException {
+        return node.getProperty("answerVal").getValue().getString();
+    }
+
+    public Map getNumberOfVoteForEachAnswer() {
+        return root;
+    }
+}
